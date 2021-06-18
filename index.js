6e34eb9fc04ff1051ee9a9044be77e39171e775e
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-18 00:01:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,19 +2,15 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
-  return kittens
 }
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
-  return kittens
 }
 function destructivelyRemoveLastKitten(name) {
   kittens.pop(name)
-  return kittens
 }
 function destructivelyRemoveFirstKitten(name) {
   kittens.shift(name)
-  return kittens
 }
 function appendKitten(name) {
 return [...kittens, name]
